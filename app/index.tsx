2d19add61013b60ffef602608306a013967aa51a
franco3445 <francoappss@gmail.com>
2025-05-13 20:58:26 -0500

    add top bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import TopBar from '@/app/components/TopBar';
+import { View } from 'react-native';
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/test.tsx to edit this screen.</Text>
+    <View>
+        <TopBar/>
     </View>
   );
 }
