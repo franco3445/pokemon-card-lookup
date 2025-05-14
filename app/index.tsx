79f8f3a7e82653ff700aca62f737b371216f39de
franco3445 <francoappss@gmail.com>
2025-05-13 22:00:11 -0500

    logs pokemon on search

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
 import TopBar from '@/app/components/TopBar';
-import React from 'react';
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import React, {useEffect} from 'react';
 import { View } from 'react-native';
 
 export default function Index() {
     const [ searchTerm, setSearchTerm ] = React.useState('');
+
     const handleSearchTermChange = async function (event) {
-        setSearchTerm(event.target.value);
-    }
+        if ( event.target.value != searchTerm ){
+            setSearchTerm(event.target.value);
+        }
+    };
+
+    const search =  async function (term) {
+        const query = `name:${term}`;
+        const paramsV2: PokemonTCG.Parameter = { q: query };
+
+        const cards = await PokemonTCG.findCardsByQueries(paramsV2)
+        console.log(cards);
+    };
+
+    useEffect(() => {
+        if (searchTerm != '') {
+            search(searchTerm)
+                .then(r => console.log('search complete'))
+                .catch(err => console.log(err));
+        }
+    }, [searchTerm]);
 
     return (
         <View>
