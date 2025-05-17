eeec0f835401c3315b4a85974a957fe6222ed71c
franco3445 <francoappss@gmail.com>
2025-05-17 14:32:11 -0500

    add spinner

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,8 @@
 import React, {useEffect} from 'react';
+import {
+    Backdrop,
+    CircularProgress,
+} from '@mui/material/';
 
 import CardDisplay from "@/app/components/CardDisplay";
 import TopBar from '@/app/components/TopBar';
@@ -8,8 +12,17 @@ import { Card as PokemonCard } from '@/app/types/card';
 export default function Index() {
     const [ searchTerm, setSearchTerm ] = React.useState('');
     const [ retrievedCards, setRetrievedCards ] = React.useState<PokemonCard[]>([]);
+    const [loading, setLoading] = React.useState(false);
+
+    const handleClose = () => {
+        setLoading(false);
+    };
+    const handleToggle = () => {
+        setLoading(!loading);
+    };
 
     const handleSearchTermChange = async function (event: React.ChangeEvent, value: string): void {
+        handleToggle();
         setSearchTerm(value);
     };
 
@@ -18,6 +31,7 @@ export default function Index() {
             .then(cards => {
                 console.log(cards);
                 setRetrievedCards(cards);
+                handleClose();
             })
             .catch(err => console.log(err));
     }, [searchTerm]);
@@ -27,6 +41,13 @@ export default function Index() {
             <TopBar
                 handleSearchTermChange={handleSearchTermChange}
             />
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}
+                onClick={handleClose}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
             <CardDisplay
                 cards={retrievedCards}
             />
